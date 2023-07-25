fd879bac54230f6a5cbbf49457121ab0e1b03f9a
tjfehdgns1 <tjfehdgns@gmail.com>
2023-07-25 10:28:06 +0900

    ?

--- common/jumpbox.gd
+++ common/jumpbox.gd
@@ -7,5 +7,5 @@ signal bounce(object)
 func _on_body_entered(player: Player) -> void:
 	if PlayerStats.resource < PlayerStats.max_resource:
 		PlayerStats.resource = PlayerStats.max_resource
-		Sounds.play(Sounds.reload,1.0,-5.0)
+		Sounds.play(Sounds.reload)
 	bounce.emit(player)
